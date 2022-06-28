5ee48dd390a920aeb7e7c2f6b251955c04db87fd
Ishan Parlikar <iparlika@emich.edu>
2022-06-29 00:09:23 +0530

    Initial Commit -- Svelte Boilerplate(Tailwind, DaisyUI)

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '../app.css';
+	import Navbar from '../components/Navbar.svelte';
+</script>
+
+<template>
+	<div class="container mx-auto">
+		<Navbar />
+		<slot />
+	</div>
+</template>
